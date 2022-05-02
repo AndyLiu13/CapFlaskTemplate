ae66527627754e2f0cb4915f84784d2b3e008dfc
AndyLiu13 <s_andy.liu3@ousd.org>
2022-05-02 10:20:23 -0700

    git remote -vdferf

--- app/routes/default.py
+++ app/routes/default.py
@@ -13,3 +13,60 @@ def test():
 @app.route('/aboutus')
 def aboutus():
     return render_template('aboutus.html')
+
+@app.route('/Climate')
+def Climate():
+    return render_template('Climate.html')
+
+@app.route('/Solution')
+def Solution():
+    return render_template('Solution.html')
+
+@app.route('/youth')
+def youth():
+    return render_template('youth.html')
+
+@app.route('/intro')
+def intro():
+    return render_template('intro.html')
+
+@app.route('/game')
+def game():
+    return render_template('game.html')
+@app.route('/mars')
+def mars():
+    return render_template('mars.html')
+
+@app.route('/earth')
+def earth():
+    return render_template('earth.html')
+@app.route('/city')
+def city():
+    return render_template('city.html')
+@app.route('/village')
+def village():
+    return render_template('village.html')
+@app.route('/good')
+def good():
+    return render_template('goodcar.html')
+@app.route('/bad')
+def bad():
+    return render_template('badcar.html')
+@app.route('/school')
+def school():
+    return render_template('school.html')
+@app.route('/result')
+def result():
+    return render_template('results.html')
+@app.route('/gasresult')
+def gasresult():
+    return render_template('gasresult.html')
+@app.route('/gascar')
+def gascar():
+    return render_template('gascar.html')
+@app.route('/driveresult')
+def driveresult():
+    return render_template('driveresult.html')
+@app.route('/endresult')
+def endresult():
+    return render_template('endresult.html')
